b37073e5898df336b98b0d1ca1c61bb72d2b0856
alkozai571 <abdulbaheeralkozai@gmail.com>
2025-12-25 22:53:51 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,552 @@
+const kitapVerisi = {
+
+"Macera": [
+{
+ad:"Define Adası",
+yazar:"	Robert Louis Stevenson",
+kapak:"kapaklar/macera1.jpg",
+ozet:{
+"8-10":"Jim’in korsanlarla dolu eğlenceli macerası Eser birçok kez sinemaya aktarılmıştır. Ayrıca Define Adasına Dönüş adındaki İngiliz televizyon dizisine esin kaynağı olmuştur .",
+"11-13":"Gizemli harita etrafında gelişen heyecanlı bir serüven.",
+"14-16":"Macera, dostluk ve ihanet temalarını işler.",
+"17-19":"Tehlike karşısında insan davranışlarını sorgular.",
+"20+":"Güç, ahlak ve liderlik üzerine alegorik okumalar sunar."
+}},
+{
+ad:"Robinson Crusoe",
+kapak:"kapaklar/macera2.jpg",
+yazar:"",
+ozet:{
+"8-10":"Issız adada hayatta kalma hikâyesi.",
+"11-13":"Doğayla mücadele eden bir insanın öyküsü.",
+"14-16":"Yalnızlık ve irade temaları ön planda.",
+"17-19":"Medeniyet ve doğa çatışmasını işler.",
+"20+":"Modern bireyin yalnızlığına dair felsefi bir eser."
+}},
+{
+ad:"80 Günde Devri Âlem",
+kapak:"kapaklar/macera3.jpg",
+yazar:"",
+ozet:{
+"8-10":"Dünya turu yapan bir adamın macerası.",
+"11-13":"Zamana karşı yarışılan heyecanlı yolculuk.",
+"14-16":"Kültürler arası keşif ve sabır teması.",
+"17-19":"Modernleşme ve hız kavramını sorgular.",
+"20+":"Sanayi toplumu eleştirisi içerir."
+}},
+{
+ad:"Tom Sawyer",
+kapak:"kapaklar/macera4.jpg",
+ozet:{
+"8-10":"Yaramaz bir çocuğun eğlenceli maceraları.",
+"11-13":"Özgürlük ve arkadaşlık temalı hikâye.",
+"14-16":"Toplumsal normlara eleştirel bakış.",
+"17-19":"Bireysel özgürlük arayışı anlatılır.",
+"20+":"Amerikan toplum yapısını yansıtır."
+}},
+{
+ad:"Gulliver’in Gezileri",
+kapak:"kapaklar/macera5.jpg",
+ozet:{
+"8-10":"Devler ve minik insanlar ülkesi.",
+"11-13":"Farklı ülkelerde geçen ilginç yolculuklar.",
+"14-16":"Toplumları eleştiren fantastik anlatım.",
+"17-19":"İnsan doğasına hicivli yaklaşım.",
+"20+":"Siyasi ve toplumsal taşlamadır."
+}},
+{
+ad:"Beyaz Diş",
+kapak:"kapaklar/macera6.jpg",
+ozet:{
+"8-10":"Bir kurt köpeğinin hikâyesi.",
+"11-13":"Doğa ve insan ilişkisi anlatılır.",
+"14-16":"Güç ve hayatta kalma teması.",
+"17-19":"Vahşilik ve uygarlık çatışması.",
+"20+":"Doğal seçilim üzerine alegorik okuma."
+}},
+{
+ad:"Moby Dick",
+kapak:"kapaklar/macera7.jpg",
+ozet:{
+"8-10":"Dev bir balinanın peşindeki gemi.",
+"11-13":"Denizde geçen tehlikeli bir yolculuk.",
+"14-16":"Takıntı ve insan iradesi işlenir.",
+"17-19":"İnsan-doğa çatışması derinleşir.",
+"20+":"Varoluşsal ve sembolik anlatım."
+}},
+{
+ad:"Yeraltı Dünyasına Yolculuk",
+kapak:"kapaklar/macera8.jpg",
+ozet:{
+"8-10":"Yer altına yapılan ilginç keşif.",
+"11-13":"Bilimsel macera dolu bir yolculuk.",
+"14-16":"Merak ve keşif duygusu öne çıkar.",
+"17-19":"Bilimin sınırlarını sorgular.",
+"20+":"Pozitivist bilim anlayışını yansıtır."
+}},
+{
+ad:"Heidi",
+kapak:"kapaklar/macera9.jpg",
+ozet:{
+"8-10":"Dağlarda yaşayan sevimli bir kız.",
+"11-13":"Doğa sevgisi ve dostluk anlatılır.",
+"14-16":"Saflık ve şehir–doğa karşıtlığı.",
+"17-19":"Toplumsal uyum teması işlenir.",
+"20+":"İnsanın doğayla ilişkisini anlatır."
+}},
+{
+ad:"Pollyanna",
+kapak:"kapaklar/macera10.jpg",
+ozet:{
+"8-10":"Mutlu olma oyunu oynayan bir kız.",
+"11-13":"İyimserliğin gücü anlatılır.",
+"14-16":"Pozitif düşünmenin etkileri.",
+"17-19":"Psikolojik dayanıklılık teması.",
+"20+":"Pozitif psikoloji örneğidir."
+}}
+],
+
+"Bilim Kurgu": [
+{ ad:"Zaman Makinesi", kapak:"kapaklar/bilim1.jpg",
+ozet:{
+"8-10":"Zaman yolculuğu macerası.",
+"11-13":"Geleceğe yapılan ilginç seyahat.",
+"14-16":"Toplumların evrimi anlatılır.",
+"17-19":"Sınıf ayrımı eleştirisi.",
+"20+":"Ütopya–distopya felsefesi."
+}},
+{ ad:"Dune", kapak:"kapaklar/bilim2.jpg",
+ozet:{
+"8-10":"Çöl gezegeninde geçen hikâye.",
+"11-13":"Güç ve kehanet teması.",
+"14-16":"Siyaset ve ekoloji.",
+"17-19":"İktidar ilişkileri.",
+"20+":"Derin politik alegori."
+}},
+{ ad:"Ben, Robot", kapak:"kapaklar/bilim3.jpg",
+ozet:{
+"8-10":"Akıllı robot hikâyeleri.",
+"11-13":"Robotların kuralları.",
+"14-16":"İnsan–makine ilişkisi.",
+"17-19":"Etik ve teknoloji.",
+"20+":"Yapay zekâ felsefesi."
+}},
+{ ad:"Fahrenheit 451", kapak:"kapaklar/bilim4.jpg",
+ozet:{
+"8-10":"Kitapların yasak olduğu dünya.",
+"11-13":"Bilginin önemi anlatılır.",
+"14-16":"Baskıcı toplum eleştirisi.",
+"17-19":"Sansür ve özgürlük.",
+"20+":"Totaliter rejim eleştirisi."
+}},
+{ ad:"1984", kapak:"kapaklar/bilim5.jpg",
+ozet:{
+"8-10":"Gözetlenen bir dünya.",
+"11-13":"Büyük Birader kavramı.",
+"14-16":"Özgürlük ve baskı.",
+"17-19":"Totalitarizm analizi.",
+"20+":"Modern siyaset eleştirisi."
+}},
+{ ad:"Cesur Yeni Dünya", kapak:"kapaklar/bilim6.jpg",
+ozet:{
+"8-10":"Farklı bir gelecek dünyası.",
+"11-13":"Kontrollü toplum.",
+"14-16":"Bireysellik sorgulanır.",
+"17-19":"Mutluluk–özgürlük çatışması.",
+"20+":"Teknokratik distopya."
+}},
+{ ad:"Mars Yıllıkları", kapak:"kapaklar/bilim7.jpg",
+ozet:{
+"8-10":"Mars’ta geçen öyküler.",
+"11-13":"Uzay kolonileri.",
+"14-16":"İnsanlık eleştirisi.",
+"17-19":"Sömürgecilik metaforu.",
+"20+":"Toplumsal alegori."
+}},
+{ ad:"Otomatik Portakal", kapak:"kapaklar/bilim8.jpg",
+ozet:{
+"8-10":"Gençlerin dünyası.",
+"11-13":"Şiddetin sonuçları.",
+"14-16":"Özgür irade tartışması.",
+"17-19":"Davranış kontrolü.",
+"20+":"Ahlak felsefesi."
+}},
+{ ad:"Uzayda Piknik", kapak:"kapaklar/bilim9.jpg",
+ozet:{
+"8-10":"Gizemli uzay alanı.",
+"11-13":"Bilinmeyen varlıklar.",
+"14-16":"İnsan merakı.",
+"17-19":"Bilim ve etik.",
+"20+":"Varoluşsal bilim kurgu."
+}},
+{ ad:"Yıldız Günlüğü", kapak:"kapaklar/bilim10.jpg",
+ozet:{
+"8-10":"Komik uzay hikâyeleri.",
+"11-13":"Farklı gezegenler.",
+"14-16":"Bilimle mizah.",
+"17-19":"İnsanlık eleştirisi.",
+"20+":"Felsefi hiciv."
+}}
+]
+,"Roman":[
+{ad:"Suç ve Ceza",kapak:"kapaklar/roman1.jpg",ozet:{
+"8-10":"İyilik ve kötülük üzerine bir hikâye.",
+"11-13":"Bir suçun sonuçları anlatılır.",
+"14-16":"Vicdan ve pişmanlık ön plandadır.",
+"17-19":"Ahlak ve birey çatışması işlenir.",
+"20+":"Derin psikolojik çözümlemeler içerir."
+}},
+{ad:"Sefiller",kapak:"kapaklar/roman2.jpg",ozet:{
+"8-10":"İyilik yapan bir adamın öyküsü.",
+"11-13":"Adalet ve merhamet teması.",
+"14-16":"Toplumsal eşitsizlikler anlatılır.",
+"17-19":"İnsan onuru ve vicdan sorgulanır.",
+"20+":"Sosyal adalet eleştirisi sunar."
+}},
+{ad:"Çalıkuşu",kapak:"kapaklar/roman3.jpg",ozet:{
+"8-10":"Cesur bir öğretmenin hikâyesi.",
+"11-13":"Anadolu’da geçen duygusal yaşam.",
+"14-16":"Kadın ve toplum ilişkisi.",
+"17-19":"Modernleşme süreci ele alınır.",
+"20+":"Toplumsal dönüşüm romanıdır."
+}},
+{ad:"Kürk Mantolu Madonna",kapak:"kapaklar/roman4.jpg",ozet:{
+"8-10":"Sessiz bir aşk hikâyesi.",
+"11-13":"Duygusal bağlar anlatılır.",
+"14-16":"İçe dönük birey anlatımı.",
+"17-19":"Yabancılaşma teması.",
+"20+":"Psikolojik derinliklidir."
+}},
+{ad:"Fareler ve İnsanlar",kapak:"kapaklar/roman5.jpg",ozet:{
+"8-10":"İki arkadaşın dostluğu.",
+"11-13":"Hayaller ve zorluklar.",
+"14-16":"Yalnızlık teması.",
+"17-19":"Toplumsal dışlanma.",
+"20+":"Amerikan rüyası eleştirisi."
+}},
+{ad:"Anna Karenina",kapak:"kapaklar/roman6.jpg",ozet:{
+"8-10":"Aile yaşamı anlatılır.",
+"11-13":"Duygusal ilişkiler.",
+"14-16":"Aşk ve toplum.",
+"17-19":"Birey-toplum çatışması.",
+"20+":"Klasik psikolojik roman."
+}},
+{ad:"İnce Memed",kapak:"kapaklar/roman7.jpg",ozet:{
+"8-10":"Köyde geçen kahramanlık.",
+"11-13":"Zulme karşı duruş.",
+"14-16":"Adalet arayışı.",
+"17-19":"Toplumsal isyan.",
+"20+":"Anadolu epik romanı."
+}},
+{ad:"Tutunamayanlar",kapak:"kapaklar/roman8.jpg",ozet:{
+"8-10":"Arkadaşlık hikâyesi.",
+"11-13":"Hayata uyum sorunu.",
+"14-16":"Bireysel sorgulama.",
+"17-19":"Modern insan bunalımı.",
+"20+":"Postmodern başyapıt."
+}},
+{ad:"Simyacı",kapak:"kapaklar/roman9.jpg",ozet:{
+"8-10":"Hayallerin peşinden gitmek.",
+"11-13":"Kişisel yolculuk.",
+"14-16":"Kader ve inanç.",
+"17-19":"İçsel keşif.",
+"20+":"Felsefi alegori."
+}},
+{ad:"Don Kişot",kapak:"kapaklar/roman10.jpg",ozet:{
+"8-10":"Komik bir şövalye.",
+"11-13":"Hayal gücü.",
+"14-16":"Gerçeklik sorgusu.",
+"17-19":"İdealizm eleştirisi.",
+"20+":"Modern romanın başlangıcı."
+}}
+]
+,"Hikaye-Öykü":[
+{ad:"Kaşağı",kapak:"kapaklar/hikaye1.jpg",ozet:{
+"8-10":"Kardeşlik hikâyesi.",
+"11-13":"Suçluluk duygusu.",
+"14-16":"Vicdan muhasebesi.",
+"17-19":"Ahlaki dersler.",
+"20+":"Psikolojik çözümleme."
+}},
+{ad:"Falaka",kapak:"kapaklar/hikaye2.jpg",ozet:{
+"8-10":"Okul anıları.",
+"11-13":"Disiplin kavramı.",
+"14-16":"Eğitim eleştirisi.",
+"17-19":"Toplumsal yapı.",
+"20+":"Anı-öykü sentezi."
+}},
+{ad:"Diyet",kapak:"kapaklar/hikaye3.jpg",ozet:{
+"8-10":"Sadakat hikâyesi.",
+"11-13":"Vefa duygusu.",
+"14-16":"İnsan ilişkileri.",
+"17-19":"Ahlaki sorumluluk.",
+"20+":"Toplumsal analiz."
+}},
+{ad:"Pembe İncili Kaftan",kapak:"kapaklar/hikaye4.jpg",ozet:{
+"8-10":"Cesur bir elçi.",
+"11-13":"Onur ve gurur.",
+"14-16":"Devlet bilinci.",
+"17-19":"Milli değerler.",
+"20+":"Tarihsel hikâye."
+}},
+{ad:"Beyaz Gemi",kapak:"kapaklar/hikaye5.jpg",ozet:{
+"8-10":"Çocuk hayalleri.",
+"11-13":"Masumiyet.",
+"14-16":"Doğa sevgisi.",
+"17-19":"Yabancılaşma.",
+"20+":"Sembolik anlatım."
+}},
+{ad:"Son Kuşlar",kapak:"kapaklar/hikaye6.jpg",ozet:{
+"8-10":"Doğa hikâyeleri.",
+"11-13":"Çevre bilinci.",
+"14-16":"İnsan-doğa ilişkisi.",
+"17-19":"Ekolojik duyarlılık.",
+"20+":"Toplumsal eleştiri."
+}},
+{ad:"Semaver",kapak:"kapaklar/hikaye7.jpg",ozet:{
+"8-10":"Aile sıcaklığı.",
+"11-13":"Günlük yaşam.",
+"14-16":"Toplumsal gözlem.",
+"17-19":"Sade insan portresi.",
+"20+":"Gerçekçi öykü."
+}},
+{ad:"Alemdağ’da Var Bir Yılan",kapak:"kapaklar/hikaye8.jpg",ozet:{
+"8-10":"Gizemli anlatım.",
+"11-13":"Merak uyandırır.",
+"14-16":"Modern birey.",
+"17-19":"Bunalım teması.",
+"20+":"Modernist öykü."
+}},
+{ad:"Gecenin Sonuna Yolculuk",kapak:"kapaklar/hikaye9.jpg",ozet:{
+"8-10":"Macera anlatısı.",
+"11-13":"Zorlu yolculuk.",
+"14-16":"İnsan psikolojisi.",
+"17-19":"Savaş eleştirisi.",
+"20+":"Varoluşsal roman."
+}},
+{ad:"Korkuyu Beklerken",kapak:"kapaklar/hikaye10.jpg",ozet:{
+"8-10":"Korku duygusu.",
+"11-13":"Gerilimli anlatım.",
+"14-16":"İç çatışma.",
+"17-19":"Yabancılaşma.",
+"20+":"Psikolojik derinlik."
+}}
+]
+,"Masal-Efsane":[
+{ad:"Keloğlan Masalları",kapak:"kapaklar/masal1.jpg",ozet:{
+"8-10":"Zeki ve eğlenceli Keloğlan masalları.",
+"11-13":"Akıl ve dürüstlükle kazanılan başarılar.",
+"14-16":"Halk kültürü ve değerler anlatılır.",
+"17-19":"Toplumsal mesajlar içeren masallar.",
+"20+":"Sözlü kültürün önemli örneklerindendir."
+}},
+{ad:"Binbir Gece Masalları",kapak:"kapaklar/masal2.jpg",ozet:{
+"8-10":"Şehrazat’ın büyülü hikâyeleri.",
+"11-13":"Macera ve hayal gücü dolu masallar.",
+"14-16":"Doğu kültürü ve anlatı geleneği.",
+"17-19":"İnsan doğası ve ahlak temaları.",
+"20+":"Çerçeve anlatım tekniğinin başyapıtıdır."
+}},
+{ad:"Ezop Masalları",kapak:"kapaklar/masal3.jpg",ozet:{
+"8-10":"Hayvanlar üzerinden anlatılan dersler.",
+"11-13":"Ahlaki öğütler içeren kısa hikâyeler.",
+"14-16":"Sembolizm ve mesajlar.",
+"17-19":"Toplumsal davranış eleştirisi.",
+"20+":"Didaktik anlatımın klasik örneği."
+}},
+{ad:"La Fontaine Masalları",kapak:"kapaklar/masal4.jpg",ozet:{
+"8-10":"Hayvan karakterli eğlenceli masallar.",
+"11-13":"Dürüstlük ve çalışkanlık temaları.",
+"14-16":"İnsan davranışları eleştirilir.",
+"17-19":"Toplumsal dersler barındırır.",
+"20+":"Fablların en bilinen örneklerindendir."
+}},
+{ad:"Dede Korkut Hikâyeleri",kapak:"kapaklar/masal5.jpg",ozet:{
+"8-10":"Cesur kahramanların öyküleri.",
+"11-13":"Türk kültürü ve değerleri.",
+"14-16":"Aile, cesaret ve sadakat.",
+"17-19":"Toplumsal yapı ve gelenekler.",
+"20+":"Türk destansı anlatımının temeli."
+}},
+{ad:"Nasreddin Hoca Fıkraları",kapak:"kapaklar/masal6.jpg",ozet:{
+"8-10":"Komik ve düşündürücü fıkralar.",
+"11-13":"Akıl dolu mizah örnekleri.",
+"14-16":"Toplumsal eleştiriler.",
+"17-19":"İroni ve bilgelik.",
+"20+":"Halk mizahının güçlü temsilcisi."
+}},
+{ad:"Andersen Masalları",kapak:"kapaklar/masal7.jpg",ozet:{
+"8-10":"Duygusal ve hayal dolu masallar.",
+"11-13":"İyilik ve fedakârlık teması.",
+"14-16":"İnsan duyguları ön planda.",
+"17-19":"Toplumsal yalnızlık anlatılır.",
+"20+":"Evrensel masal anlatımı sunar."
+}},
+{ad:"Grimm Masalları",kapak:"kapaklar/masal8.jpg",ozet:{
+"8-10":"Klasik peri masalları.",
+"11-13":"İyiler ve kötüler.",
+"14-16":"Masalların karanlık yüzü.",
+"17-19":"Psikolojik alt metinler.",
+"20+":"Avrupa masal geleneği."
+}},
+{ad:"Türk Efsaneleri",kapak:"kapaklar/masal9.jpg",ozet:{
+"8-10":"Doğa ve kahraman efsaneleri.",
+"11-13":"Halk inanışları.",
+"14-16":"Mitolojik öğeler.",
+"17-19":"Kültürel kimlik.",
+"20+":"Sözlü tarih kaynağı."
+}},
+{ad:"Mitolojik Masallar",kapak:"kapaklar/masal10.jpg",ozet:{
+"8-10":"Tanrılar ve kahramanlar.",
+"11-13":"Mitolojik hikâyeler.",
+"14-16":"İnsanlık mitleri.",
+"17-19":"Sembolizm ve anlam.",
+"20+":"Mitoloji incelemesi için temel."
+}}
+]
+,"Destan":[
+{ad:"Manas Destanı",kapak:"kapaklar/destan1.jpg",ozet:{
+"8-10":"Cesur bir kahramanın hikâyesi.",
+"11-13":"Kahramanlık ve mücadele.",
+"14-16":"Toplumsal birlik.",
+"17-19":"Türk destan geleneği.",
+"20+":"Sözlü kültür başyapıtı."
+}},
+{ad:"Oğuz Kağan Destanı",kapak:"kapaklar/destan2.jpg",ozet:{
+"8-10":"Büyük bir liderin hayatı.",
+"11-13":"Kahramanlık anlatısı.",
+"14-16":"Devlet kurma fikri.",
+"17-19":"Türk siyasi düşüncesi.",
+"20+":"Mitolojik tarih anlatımı."
+}},
+{ad:"Ergenekon Destanı",kapak:"kapaklar/destan3.jpg",ozet:{
+"8-10":"Demir dağı eriten Türkler.",
+"11-13":"Özgürlük mücadelesi.",
+"14-16":"Yeniden doğuş teması.",
+"17-19":"Milli bilinç.",
+"20+":"Kolektif kimlik sembolü."
+}},
+{ad:"İlyada",kapak:"kapaklar/destan4.jpg",ozet:{
+"8-10":"Savaş ve kahramanlar.",
+"11-13":"Truva Savaşı.",
+"14-16":"Onur ve savaş.",
+"17-19":"İnsan tutkuları.",
+"20+":"Batı destan geleneği."
+}},
+{ad:"Odysseia",kapak:"kapaklar/destan5.jpg",ozet:{
+"8-10":"Uzun yolculuk hikâyesi.",
+"11-13":"Macera dolu dönüş.",
+"14-16":"Zeka ve sabır.",
+"17-19":"Bireysel mücadele.",
+"20+":"Epik anlatının zirvesi."
+}},
+{ad:"Beowulf",kapak:"kapaklar/destan6.jpg",ozet:{
+"8-10":"Canavarlarla savaş.",
+"11-13":"Cesaret ve güç.",
+"14-16":"Kahramanlık ideali.",
+"17-19":"Liderlik teması.",
+"20+":"Cermen destanı."
+}},
+{ad:"Şehname",kapak:"kapaklar/destan7.jpg",ozet:{
+"8-10":"İran kahramanları.",
+"11-13":"Krallar ve savaşlar.",
+"14-16":"Tarih ve mit.",
+"17-19":"İktidar anlatısı.",
+"20+":"Doğu destan geleneği."
+}},
+{ad:"Kalevala",kapak:"kapaklar/destan8.jpg",ozet:{
+"8-10":"Büyülü kahramanlar.",
+"11-13":"Fin mitolojisi.",
+"14-16":"Doğa ve insan.",
+"17-19":"Ulusal kimlik.",
+"20+":"Avrupa destan edebiyatı."
+}},
+{ad:"Nibelungen Destanı",kapak:"kapaklar/destan9.jpg",ozet:{
+"8-10":"Ejderha ve kahraman.",
+"11-13":"İhanet ve güç.",
+"14-16":"Trajik kader.",
+"17-19":"İntikam teması.",
+"20+":"Alman destanı."
+}},
+{ad:"Alpamış Destanı",kapak:"kapaklar/destan10.jpg",ozet:{
+"8-10":"Cesur Alpamış.",
+"11-13":"Aşk ve kahramanlık.",
+"14-16":"Toplumsal değerler.",
+"17-19":"Türk destan geleneği.",
+"20+":"Orta Asya kültürü."
+}}
+]
+,"Gezi":[
+{ad:"Seyahatname",kapak:"kapaklar/gezi1.jpg",ozet:{
+"8-10":"Gezilen yerlerin hikâyesi.",
+"11-13":"Farklı ülkeler tanıtılır.",
+"14-16":"Kültürel gözlemler.",
+"17-19":"Toplumsal yapı analizi.",
+"20+":"Tarihsel gezi edebiyatı."
+}},
+{ad:"İzlanda Günlükleri",kapak:"kapaklar/gezi2.jpg",ozet:{
+"8-10":"Soğuk ülke macerası.",
+"11-13":"Doğa ve keşif.",
+"14-16":"Farklı yaşamlar.",
+"17-19":"Coğrafi gözlemler.",
+"20+":"Modern gezi yazısı."
+}},
+{ad:"Afrika Gezi Notları",kapak:"kapaklar/gezi3.jpg",ozet:{
+"8-10":"Afrika’yı tanıyalım.",
+"11-13":"Farklı kültürler.",
+"14-16":"İnsan yaşamı.",
+"17-19":"Sömürge sonrası toplum.",
+"20+":"Antropolojik gözlem."
+}},
+{ad:"Kutup Günlükleri",kapak:"kapaklar/gezi4.jpg",ozet:{
+"8-10":"Buzlar ülkesi.",
+"11-13":"Zorlu doğa koşulları.",
+"14-16":"İnsan ve doğa.",
+"17-19":"Keşif ruhu.",
+"20+":"Bilimsel seyahat anlatısı."
+}},
+{ad:"Anadolu Notları",kapak:"kapaklar/gezi5.jpg",ozet:{
+"8-10":"Türkiye’yi tanıyalım.",
+"11-13":"Şehirler ve insanlar.",
+"14-16":"Kültürel zenginlik.",
+"17-19":"Toplumsal yapı.",
+"20+":"Anadolu kültürü."
+}},
+{ad:"Uzakdoğu Seyahati",kapak:"kapaklar/gezi6.jpg",ozet:{
+"8-10":"Uzak ülkeler.",
+"11-13":"Farklı gelenekler.",
+"14-16":"Kültür karşılaştırması.",
+"17-19":"Modernleşme etkileri.",
+"20+":"Küresel bakış."
+}},
+{ad:"Avrupa Günlüğü",kapak:"kapaklar/gezi7.jpg",ozet:{
+"8-10":"Avrupa şehirleri.",
+"11-13":"Tarihi mekânlar.",
+"14-16":"Kültür turu.",
+"17-19":"Toplum karşılaştırması.",
+"20+":"Avrupa medeniyeti."
+}},
+{ad:"Asya Seyahatnamesi",kapak:"kapaklar/gezi8.jpg",ozet:{
+"8-10":"Asya’yı tanıyalım.",
+"11-13":"Geleneksel yaşam.",
+"14-16":"Farklı kültürler.",
+"17-19":"Toplumsal yapı.",
+"20+":"Kültürel inceleme."
+}},
+{ad:"Amerika Günlükleri",kapak:"kapaklar/gezi9.jpg",ozet:{
+"8-10":"Büyük şehirler.",
+"11-13":"Modern yaşam.",
+"14-16":"Şehir kültürü.",
+"17-19":"Tüketim toplumu.",
+"20+":"Modern dünya eleştirisi."
+}},
+{ad:"Dünya Atlası Gezi Yazıları",kapak:"kapaklar/gezi10.jpg",ozet:{
+"8-10":"Dünyayı tanıyalım.",
+"11-13":"Ülkeler ve insanlar.",
+"14-16":"Coğrafya bilgisi.",
+"17-19":"Küresel farkındalık.",
+"20+":"Dünya kültürleri."
+}}
+]
+
+};
